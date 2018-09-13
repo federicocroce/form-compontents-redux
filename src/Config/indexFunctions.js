827a36782200fe6317124bc2e6b3ba756abe63a9
Federico Croce <federico.croce@bbva.com>
2018-09-13 16:54:27 -0300

    Cambio en la validacion de isUndefinedOrNullOrEmpty

--- src/Config/indexFunctions.js
+++ src/Config/indexFunctions.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import $ from 'jquery-lite';
-import _ from "lodash";
 import ReactJson from 'react-json-view';
 
 const divStyle = {margin: '20px 0'};
 
 const functions = {};
 
-functions.isUndefinedOrNullOrEmpty = (element) => typeof(element) != "function" ? _.isEmpty(element) : false || element == null || element == undefined  ? true : false;
+functions.isFunction = (e) => typeof(e) == "function";
+
+functions.isEmpty = (e) =>  e == "" || JSON.stringify(e) === '{}' || e.length == 0   ? true : false;
+functions.isUndefinedOrNull = (e) =>   e == null || e == undefined ? true : false;
+
+
+functions.isUndefinedOrNullOrEmpty = (e) => !functions.isFunction(e)  ? functions.isUndefinedOrNull(e) : false || functions.isEmpty(e) ? true : false;
+
 functions.jsonView = (json) => <div style={divStyle}> <ReactJson  src={json} theme="monokai" /> </div>;
 
 
